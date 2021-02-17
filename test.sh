d60165b39227242d3dc0313b3e70e74361dc094a
swen2003 <78562196+swen2003@users.noreply.github.com>
2021-02-17 15:45:07 +0700

    fill bytes

--- test.sh
+++ test.sh
@@ -4,6 +4,8 @@ git pull
 git add .
 git commit -m "fill bytes"
 git push --force origin main
+printf "tunggu 1 menit setiap 1 commit"
+sleep 60
 
 touch "repo1/spamfile.txt"
 echo "spam " >> "repo1/spamfile.txt"
@@ -11,6 +13,8 @@ git pull
 git add .
 git commit -m "fill bytes"
 git push --force origin main
+printf "tunggu 1 menit setiap 1 commit"
+sleep 60
 
 touch "repo1/spamfile.txt"
 echo "spam " >> "repo1/spamfile.txt"
@@ -18,6 +22,8 @@ git pull
 git add .
 git commit -m "fill bytes"
 git push --force origin main
+printf "tunggu 1 menit setiap 1 commit"
+sleep 60
 
 touch "repo1/spamfile.txt"
 echo "spam " >> "repo1/spamfile.txt"
@@ -25,6 +31,8 @@ git pull
 git add .
 git commit -m "fill bytes"
 git push --force origin main
+printf "tunggu 1 menit setiap 1 commit"
+sleep 60
 
 touch "repo1/spamfile.txt"
 echo "spam " >> "repo1/spamfile.txt"
@@ -32,3 +40,6 @@ git pull
 git add .
 git commit -m "fill bytes"
 git push --force origin main
+printf "akan di lanjutkan dalam 55 menit"
+sleep 3300
+start test1.sh
